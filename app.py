dc0e0b5b4fe7353193cbd0cac72d85964431921d
prikakade-lgtm <prikakade@gmail.com>
2026-04-17 01:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ elif st.session_state.page == "final":
     col1, col2 = st.columns(2)
 
     with col1:
-        if st.button("call her 📞", key="call_btn"):
+        if st.button("call me 📞", key="call_btn"):
             st.session_state.page = "call"
 
     with col2:
